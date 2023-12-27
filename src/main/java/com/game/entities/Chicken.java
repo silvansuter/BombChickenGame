f5b64ce41add1dfe0fb3d7a31c990e52f87f67bc
Silvan Suter <sili.suter@bluewin.ch>
2023-12-27 10:26:16 +0100

    removed unnecessary import

--- src/main/java/com/game/entities/Chicken.java
+++ src/main/java/com/game/entities/Chicken.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
 
 public class Chicken extends Entity {
     private int speedX;
